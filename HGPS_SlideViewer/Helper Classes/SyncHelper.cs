218da458b105be8c4185129f2b959e1af486dc37
Ho Xuan Vuong <kiepcode1998@gmail.com>
2020-06-23 11:28:15 +0800

    change http to https to meet SSL

--- HGPS_SlideViewer/Helper Classes/SyncHelper.cs
+++ HGPS_SlideViewer/Helper Classes/SyncHelper.cs
@@ -13,8 +13,8 @@ namespace HGPS_SlideViewer
         private static HubConnection _hubConnection;
         private static IHubProxy _syncHub, _myHub;
         private const string CLIENT_NAME = "Robo-TA Slides";
-        //private const string _baseAddress = "http://robo-ta.com/";
-        private const string _baseAddress = "https://localhost:44353/";
+        private const string _baseAddress = "https://robo-ta.com/";
+        //private const string _baseAddress = "https://localhost:44353/";
 
         public static event EventHandler<StatusEventArgs> StatusChanged;
 
